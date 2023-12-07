1835a9f2c609dbdd2f3ab1fce216b8fbab50e578
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-12-07 08:51:38 +0100

    chore: fetch weather data

--- src/App.js
+++ src/App.js
@@ -35,12 +35,20 @@ function formatDay(dateStr) {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { location: "lagos" };
+    this.state = {
+      location: "lagos",
+      isLoading: false,
+      displayLocation: "",
+      weather: {},
+    };
     this.fetchWeather = this.fetchWeather.bind(this);
   }
 
   async fetchWeather() {
     try {
+      // Set Loading state
+      this.setState({ isLoading: true });
+
       // 1) Getting location (geocoding)
       const geoRes = await fetch(
         `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
@@ -52,16 +60,20 @@ class App extends React.Component {
 
       const { latitude, longitude, timezone, name, country_code } =
         geoData.results.at(0);
-      console.log(`${name} ${convertToFlag(country_code)}`);
+      this.setState({
+        displayLocation: `${name} ${convertToFlag(country_code)}`,
+      });
 
       // 2) Getting actual weather
       const weatherRes = await fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
       );
       const weatherData = await weatherRes.json();
-      console.log(weatherData.daily);
+      this.setState({ weather: weatherData.daily });
     } catch (err) {
       console.err(err);
+    } finally {
+      this.setState({ isLoading: false });
     }
   }
 
@@ -77,7 +89,9 @@ class App extends React.Component {
             onChange={(e) => this.setState({ location: e.target.value })}
           />
         </div>
-        <button onClick={this.fetchWeather}>Get Waether</button>
+        <button onClick={this.fetchWeather}>Get Weather</button>
+
+        {this.state.isLoading && <p className="loader">Loading...</p>}
       </div>
     );
   }
